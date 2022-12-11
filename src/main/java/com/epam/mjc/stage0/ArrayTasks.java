59c0993c12e1e42b3a72165d43c351dd8fc91e15
Oleksandr Khomenko <sasha.khomenko12@gmail.com>
2022-12-11 18:17:55 +0200

    done

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -1,86 +1,60 @@
 package com.epam.mjc.stage0;
 
-/**
- * Here are the tasks for working with the arrays.
- * <p>
- * The usage of any additional packages (such as java.util.*) is forbidden.
- */
 public class ArrayTasks {
-
-    /**
-     * Return a String[] array that will list all the seasons of the year, starting with winter.
-     */
     public String[] seasonsArray() {
-
+        String[] seasons = new String[]{"winter", "spring", "summer", "Autumn"};
+        return seasons;
     }
-
-    /**
-     * Generate an int[] array of consecutive positive integers
-     * starting at 1 of the given length (length parameter > 0).
-     * <p>
-     * Example:
-     * <p>
-     * length = 1  -> [1]
-     * length = 3  -> [1, 2, 3]
-     * length = 5  -> [1, 2, 3, 4, 5]
-     */
     public int[] generateNumbers(int length) {
-
+        int[] givenNumbers = new int[length];
+        for(int i = 0;i<length;i++){
+            givenNumbers[i] = i + 1;
+        }
+        return givenNumbers;
     }
-
-    /**
-     * Find the sum of all elements of the int[] array.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [1, 3, 5]   -> sum = 9
-     * arr = [5, -3, -4] -> sum = -2
-     */
     public int totalSum(int[] arr) {
-
+        int sumNumbers = 0;
+        for(int i : arr){
+            sumNumbers += i;
+        }
+        return sumNumbers;
     }
-
-    /**
-     * Return the index of the first occurrence of number in the arr array.
-     * If there is no such element in the array, return -1.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [99, -7, 102], number = -7    ->   2
-     * arr = [5, -3, -4],   number = 10    ->  -1
-     */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        int x = -1;
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i]==number) {
+                x = i;
+                break;
+            }
+        }
+        return x;
     }
-
-    /**
-     * Return the new String[] array obtained from the arr array
-     * by reversing the order of the elements.
-     * <p>
-     * Example:
-     * <p>
-     * arr = ["Bob", "Nick"]               -> ["Nick", "Bob"]
-     * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
-     */
     public String[] reverseArray(String[] arr) {
-
+        String[] newString = new String[arr.length];
+        int x = 0;
+        for(int i = arr.length-1; i >= 0; i--){
+            newString[x] = arr[i];
+            x++;
+        }
+        return newString;
     }
-
-    /**
-     * Return new int[] array obtained from arr int[] array
-     * by choosing positive numbers only.
-     * P.S. 0 is not a positive number =)
-     * <p>
-     * Example:
-     * <p>
-     * arr = [1,-2, 3]      -> [1, 3]
-     * arr = [-1, -2, -3]   -> []
-     * arr = [1, 2]         -> [1, 2]
-     */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-
+        int[] newInt1 = new int[0];
+        for (int i : arr){
+        if (i > 0){
+            if (newInt1.length > 0){
+                int[] AdditionArrayForNewInt = new int[newInt1.length + 1];
+                AdditionArrayForNewInt[AdditionArrayForNewInt.length - 1] = i;
+                System.arraycopy(newInt1, 0 , AdditionArrayForNewInt, 0, newInt1.length);
+                newInt1 = AdditionArrayForNewInt;
+            } else {
+                newInt1 = new int[1];
+                newInt1[0] = i;
+                }
+            }
+        }
+        return newInt1;
     }
-
     /**
      * Return a sorted, ragged, two-dimensional int[][] array following these rules:
      * Incoming one-dimensional arrays must be arranged in ascending order of their length;
@@ -92,6 +66,6 @@ public class ArrayTasks {
      * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
-
+        return arr;
     }
 }
